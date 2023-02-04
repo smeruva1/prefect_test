43435c5611bc44ed328dadbda13d2f611fa890d3
Sudar Meruva <meruva_s@yaho.com>
2023-02-04 16:01:15 -0500

    only change in comment

--- prefect_simple_function.py
+++ prefect_simple_function.py
@@ -2,7 +2,7 @@ from prefect import flow
 
 @flow
 def my_favorite_function():
-    print("What is your favortie number?")
+    print("What is your favortie number? ")
     return 42
 
 print(my_favorite_function())
